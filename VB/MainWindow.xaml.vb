28d5d7f5da16e621f2107301ccd82f05ced188fa
DevExpressExampleBot <fakeEmail>
2018-12-27 15:41:30 +0300

    Source auto update [skip ci]

---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports DevExpress.Xpf.Core
+Imports System.Collections.ObjectModel
+
+Namespace DXGridSample
+    Partial Public Class MainWindow
+        Inherits ThemedWindow
+
+        Public Sub New()
+            InitializeComponent()
+
+            Dim items = New ObservableCollection(Of Item)()
+            For i As Integer = 0 To 99
+                items.Add(New Item(i))
+            Next i
+
+            grid.ItemsSource = items
+        End Sub
+    End Class
+    Public Class Item
+        Public Sub New(ByVal i As Integer)
+            Value1 = i
+            Value2 = i
+        End Sub
+        Public Property Value1() As Decimal
+        Public Property Value2() As Decimal
+    End Class
+End Namespace
